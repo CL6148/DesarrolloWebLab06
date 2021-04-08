2fbdced69cdd82b2d94cc04e9a4f64b68dc8e5a9
CL6148 <78516398+CL6148@users.noreply.github.com>
2021-04-07 20:34:40 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
-$(document).ready(funtion() {
-  $(".agregar").click(function() {
-    $(".Lista").append('<li>Item</li>');
+$(document).ready(function(){
+  $(".agregar").click(function(){
+    var txt = $("#newText").val();
+    $(".Lista").append("<div class='lis'><li><p class='itemShop'>" + txt + "</p><button class='checar'>check</button><button class='del'>delete</button></li></div>"
+    );
+  });
+
+  $(".checar").click(function() {
+    $(this).parent().toggleClass("chec");
+  });
+
+  $(".del").click(function() {
+    $(this).parent().parent().remove();
   });
 });
