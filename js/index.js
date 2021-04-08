ca0d721f8778dccb859d5069b2006ca5a9007172
CL6148 <78516398+CL6148@users.noreply.github.com>
2021-04-07 17:25:53 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,17 +1,5 @@
 $(document).ready(funtion() {
   $(".agregar").click(function() {
-    $(".Lista").append('
-      <div class="lis">
-        <li>Item</li>
-        <p>
-          <button class="checar">
-            check
-          </button>
-          <button class="del">
-            delete
-          </button>
-        </p>
-      </div>
-    ');
+    $(".Lista").append('<li>Item</li>');
   });
 });
